97bb49c52a4b6c8b634a9b12f02a1ebf07eb57bd
yuzai chen <mvjome@gmail.com>
2009-10-21 10:45:32 +0800

    add function grep_conf

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,9 @@
+# grep a pattern which is't inincluded in .svn files
 function grep_svn {
   grep $1 -rn . | grep -v svn | less
+}
+
+# 过滤以#开头的行和空白行
+function grep_conf {
+  awk '{if(NF>0) print $0}' $1 | grep -v '^#'  | less
 }
