ae6f3f251bc373fe325f3843697d184b8180d95b
yuzai chen <chenyuzai@Macintosh.local>
2009-11-13 11:16:10 +0800

    rename zsh function grep_svn to gsvn and grep_conf to gconf

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,10 +1,10 @@
 # grep a pattern which is't inincluded in .svn files
-function grep_svn {
+function gsvn {
   grep $1 -rn . | grep -v svn | less
 }
 
 # 过滤以#开头的行和空白行
-function grep_conf {
+function gconf {
   #awk '{if(NF>0) print $0}' $1 | grep -v '^#'  | less
   #awk '!/^$/' $1 | grep -v '^#'  | less
   #awk '!/./' $1 | grep -v '^#'  | less
