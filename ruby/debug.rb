751c8de4cb2782dec87d9e425600dbac35e5ad87
yuzai chen <mvjome@gmail.com>
2010-02-21 23:41:08 +0800

    some modification on ruby/debug.rb

--- ruby/debug.rb
+++ ruby/debug.rb
@@ -1,5 +1,5 @@
 class Object
-  # self's class methods and singleton methods
+  # returns instance methods of current object's class and its singleton methods
   def lm # abbr of local methods
     self.methods.sort - self.class.superclass.methods
   end
@@ -15,23 +15,14 @@ module Kernel
   end
 
   # file utility
-  def ls
-    %x{ls -a}.split("\n")[2..-1]
-  end
-
-  def pwd
-    Dir.pwd
-  end
-
-  def clear
-    system 'clear'
-  end
-
+  def ls; %x{ls -a}.split("\n")[2..-1] end
+  def pwd; Dir.pwd end
+  def cd(dir); Dir.chdir(dir) end
+  def clear; system 'clear' end
 end
 
 unless defined? RUBY_ENGINE
   # Vi editing mode
-  require 'irb'
   IRB.conf[:USE_READLINE] = true
   def vimode; Readline.vi_editing_mode end
   def emmode; Readline.emacs_editing_mode end
