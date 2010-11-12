8adc6643b590d4bef45ed46ab809da44d55d877d
Yuzai Chen <mvjome@gmail.com>
2010-11-12 23:29:58 +0800

    Add an alias pss for "RAILS_ENV=production ruby script/server thin -u"

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -45,6 +45,7 @@ alias svn.check_ruby_syntax="ruby -r $HOME/utils/ruby/irb.rb -e 'CheckSyntax.rub
 alias t='autotest'
 alias rr='rake routes | grep'
 alias ss="ruby script/server thin -u"
+alias pss="RAILS_ENV=production ruby script/server thin -u"
 alias sc='ruby script/console'
 alias psc='RAILS_ENV=production ruby script/console'
 alias brain="o $HOME/Documents/rails_brain_2.3.4/index.html"
