c60f51094a241c695fe41554e14e35cb42bd64fd
David Chen <mvjome@gmail.com>
2012-02-27 10:33:22 +0800

    Update alias b

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -108,7 +108,7 @@ alias music="ruby -r $HOME/utils/ruby/irb.rb -e 'Music.play'"
 # ruby
 alias rubygem="cd `rvm info homes | grep 'gem:' | awk -F '"' '{print $2}'`/gems"
 alias rubylib="cd `rvm info homes | grep 'ruby:' | awk -F '"' '{print $2}'`/lib/ruby/1.8"
-alias b="bundle "
+alias b="bundle exec "
 
 
 # python
